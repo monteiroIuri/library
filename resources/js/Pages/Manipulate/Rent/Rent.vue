e1555c89fc0d85f3c485ae6854c6098e6028d0d3
monteiroIuri <iurigms@gmail.com>
2022-01-04 22:31:32 -0300

    .

--- resources/js/Pages/Manipulate/Rent/Rent.vue
+++ resources/js/Pages/Manipulate/Rent/Rent.vue
@@ -9,7 +9,7 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg grid justify-items-center">
-                    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    <div v-if="books.length" class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         <div v-for="book in books" :key="book.id" class="group relative border rounded-lg p-2 m-4">
                             <div class="w-full text-center bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
                                 <p class="text-2xl px-2"><strong>{{ book.name }}</strong></p>
@@ -34,6 +34,11 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div v-else class="mt-3 text-center grid justify-items-center">
+                        <h2 class="mb-4 font-semibold text-xl text-gray-800 leading-tight bg-grey-100">
+                            Estamos sem Livros no momento.
+                        </h2>
                     </div>
                 </div>
             </div>
